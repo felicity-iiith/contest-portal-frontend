f9fdc350bc12c6f2a92e715e5aa92cf0970a8151
Gunjan Chandani <gunjankaramchandani@gmail.com>
2017-09-24 10:42:22 +0530

    fixed indentation

--- src/Navbar/index.js
+++ src/Navbar/index.js
@@ -5,17 +5,16 @@ export default () => (
   <nav className="navigation">
     <section className="container">
       <Link className="navigation-title" to="/"><h1 className="title">Contest Portal</h1></Link>
-      
-        {!window.email ?
-          <ul className="navigation-list float-right">
+      {!window.email ?
+        <ul className="navigation-list float-right">
           <li className="navigation-item"><Link to='/login' className="navigation-link">Login</Link></li>
-          </ul>
-          :
-          <ul className="navigation-list float-right">
-            <li className="navigation-item navigation-link"> Hello {window.email} </li>
-            <li className="navigation-item"><Link to='/logout' className="navigation-link">Logout</Link></li>
-          </ul>
-        }
+        </ul>
+        :
+        <ul className="navigation-list float-right">
+          <li className="navigation-item navigation-link"> Hello {window.email} </li>
+          <li className="navigation-item"><Link to='/logout' className="navigation-link">Logout</Link></li>
+        </ul>
+      }
     
     </section>
   </nav>
