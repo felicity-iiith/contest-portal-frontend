891542310e60082e91a7f3fdbcd8e79d9a2f545c
kunalgarg2100 <kunalgarg2100@gmail.com>
2017-09-26 22:45:30 +0530

    Added Previous and Next buttons https://github.com/felicity-iiith/contest-portal-frontend/issues/1

--- src/QuestionViewer/index.js
+++ src/QuestionViewer/index.js
@@ -1,4 +1,3 @@
-/* global fetchWithAuth */
 
 import Component from 'inferno-component';
 
@@ -10,13 +9,16 @@ class QuestionViewer extends Component {
   }
   async componentDidMount() {
     const { qno } = this.props.params;
-    var res = await fetchWithAuth(`/questions/${qno}`);
+    var res = await window.fetchWithAuth(`/questions/${qno}`);
     res = await res.json();
     if (!res.error) this.setState({ question: res, loading: false })
     else this.setState({ error: res.error, loading: false })
   }
   render() {
     const { loading, question, error } = this.state
+    const quesno = parseInt(this.props.params.qno,10)
+    const prev = `/question/${quesno-1}`
+    const nxt = `/question/${quesno+1}`
     return (
       <div>
         {loading && <div>Loading...</div>}
@@ -25,7 +27,17 @@ class QuestionViewer extends Component {
         <p>
           {question.body}
         </p>
-        {/* XXX: Need to add previous and next buttons here. Use the Link component from 'inferno-router' */}
+        {
+          quesno ===1 ?(
+            <a class="button float-right" href={nxt}>Next</a>
+          ) : quesno ===5 ?(
+            <a class="button float-left" href={prev}>Prev</a>
+          )
+            :
+            (<div>
+              <a class="button float-left" href={prev}>Prev</a>
+              <a class="button float-right" href={nxt}>Next</a></div>)
+        }
       </div>
     )
   }
